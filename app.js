9f7b413effa5986192efb0fc5351dcd28c4d7899
tariqul10765 <tariqul10765@gmail.com>
2024-09-02 15:01:51 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<div style="width: 100%; height: 100vh; display: flex; flex-direction: column; align-items: center; justify-content: center"><h1 style="color: blueviolet">API RUNNING...</h1><p style="color: lightcoral">Powered by Tariqul</p></div>')
+});
+
+app.listen(4000, () => console.log(`Server is running at port:${4000}`));
