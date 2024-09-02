0762228f0a43cd4457edf06c25ca8eeb4f657a12
tariqul10765 <tariqul10765@gmail.com>
2024-09-02 16:27:51 +0600

    change something

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.send('<div style="width: 100%; height: 100vh; display: flex; flex-direction: column; align-items: center; justify-content: center"><h1 style="color: blueviolet">API RUNNING...</h1><p style="color: lightcoral">Powered by Tariqul</p></div>')
 });
 
