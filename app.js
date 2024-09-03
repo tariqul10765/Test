0ed0b9e07033e08079e9d022283acdb61f574617
tariqul10765 <tariqul10765@gmail.com>
2024-09-03 13:32:48 +0600

    added serve-index

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const serveIndex = require("serve-index");
 
 const app = express();
+
+app.use(
+    '/ftp',
+    express.static('public/ftp'),
+    serveIndex('public/ftp', { icons: true })
+)
 
 app.get('/test', (req, res) => {
     res.send('<div style="width: 100%; height: 100vh; display: flex; flex-direction: column; align-items: center; justify-content: center"><h1 style="color: blueviolet">API RUNNING...</h1><p style="color: lightcoral">Powered by Tariqul</p></div>')
